a53615acdf64abfaa5ae21aebc990f62a54ceedc
Muhammad Hamza <hamzashahid9@gamil.com>
2019-04-09 12:25:14 +0500

    lex files

---
+++ tokenizer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "tokenizer.h"
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext; 
+
+char* name[] = {NULL,"markazi","chalo","jabtak","agar","warna","agarwarna","likho","hindsa","chindsa","bhindsa","jumla","harf","aasharia","khaali","sahih","ghalat","stringliteral","identifier","decimal","integer","EA","AA","SA","MA","DA","GT","LT","GTE","LTE","IEQ","NEQ","INO","DCO","PLS","MIS","MUL","DIV","MOD","AND","OR","NOT","se","LRP","RRP","LCP","RCP", "semicolon"};
+
+int main(void){
+	int ntoken;
+	ntoken = yylex();
+	while(ntoken){
+		printf("%d  %s\n", ntoken, yytext);
+		ntoken = yylex();
+	}
+	return 0;
+}
+
